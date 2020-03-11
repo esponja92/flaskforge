ebb6f7104c4546c07a7ca32ea3090edaeafcd1e5
Hugo <hugodantas@tic.ufrj.br>
2020-03-11 16:29:30 -0300

    Implementando uma API de acesso ao banco

--- app.py
+++ app.py
@@ -1,33 +1,13 @@
 from flask import *
-import sqlite3 as sql
+from database import Database
 
 app = Flask(__name__)
-
-DATABASE = 'database.db'
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sql.connect(DATABASE)
-
-    db.row_factory = sql.Row
-    return db
-
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
-
-def execute(query, args):
-    db = get_db()
-    db.execute(query, args)
-    db.commit()
+db = Database()
 
 @app.route("/")
 def index():
 
-    pessoas = query_db("SELECT * FROM pessoa")
+    pessoas = db.obtem('pessoa')
         
     return render_template('index.html', pessoas=pessoas)
 
@@ -38,7 +18,7 @@ def cadastrar():
 
     try:
 
-        execute("INSERT INTO pessoa (fname,lname) VALUES (?,?)", (fname,lname))
+        db.insere("pessoa", ['fname', 'lname'], [fname, lname])
 
         return render_template('sucesso.html')
     except Exception as e:
@@ -47,7 +27,7 @@ def cadastrar():
 @app.route('/editar', methods=['GET'])
 def editar():
     id = request.args.get('id')
-    pessoa = query_db("SELECT * FROM pessoa WHERE id = ?", id, one=True)
+    pessoa = db.obtem('pessoa', colunas="*", where_colunas=['id'], where_valores=[id], one=True)
 
     return render_template('edit.html', pessoa=pessoa)
 
@@ -60,7 +40,7 @@ def atualizar():
 
     try:
 
-        execute("UPDATE pessoa SET fname = ?, lname = ? WHERE id = ?", (fname,lname,id))
+        db.atualiza("pessoa", ['fname', 'lname'], [fname, lname],['id'], [id])
 
         return render_template('sucesso.html')
     except Exception as e:
@@ -73,11 +53,7 @@ def deletar():
 
     try:
 
-        with sql.connect("database.db") as con:
-            cur = con.cursor()
-
-            cur.execute("DELETE FROM pessoa WHERE id = ?", id)
-            con.commit()
+        db.deleta('pessoa', where_colunas = ["id"], where_valores = [id])
 
         return render_template('sucesso.html')
     except Exception as e:
